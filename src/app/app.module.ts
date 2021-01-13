a0484d3b75bc558d928d560dc0d63c7e3c2e5262
MD Tausif Ahmad <md.ahmad@emaratech.ae>
2021-01-13 15:38:48 +0400

    Compiling as a Standalone Element

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
-import { AppComponent } from './app.component';
+import {BrowserModule} from '@angular/platform-browser';
+import {Injector, NgModule} from '@angular/core';
+import {HelloWorldComponent} from './hello-world/hello-world.component';
+import {createCustomElement} from '@angular/elements';
 
 @NgModule({
   declarations: [
-    AppComponent
+    HelloWorldComponent
   ],
   imports: [
     BrowserModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  entryComponents: [HelloWorldComponent]
 })
-export class AppModule { }
+export class AppModule {
+
+  constructor(injector: Injector) {
+    const custom = createCustomElement(HelloWorldComponent, {injector: injector});
+    customElements.define('app-hello-world', custom);
+  }
+
+  ngDoBootstrap() {}
+
+}
